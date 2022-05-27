64a0483d9c374511c8449713fbee30626eb2a770
irina <irisha.dieriecha@mail.ru>
2022-05-27 19:34:30 +0300

    refactor: changed class names according to rule 3.1 in html-and-css.md

--- app.js
+++ app.js
@@ -44,13 +44,13 @@ var createNewTaskElement=function(taskString){
     editInput.classList.add("edit-input");
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="button-edit";
+    editButton.className="button-action_edit";
     editButton.classList.add("button-action");
 
-    deleteButton.className="button-delete";
+    deleteButton.className="button-action_delete";
     deleteButton.classList.add("button-action");
     deleteButtonImg.src='./remove.svg';
-    deleteButtonImg.className = 'button-delete__img';
+    deleteButtonImg.className = 'button-action__img';
     deleteButton.appendChild(deleteButtonImg);
 
 
@@ -90,7 +90,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".button-edit");
+    var editBtn=listItem.querySelector(".button-action_edit");
     var containsClass=listItem.classList.contains("edit-task");
     //If class of the parent is .edit-task
     if(containsClass){
@@ -170,7 +170,7 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.button-edit");
+    var editButton=taskListItem.querySelector("button.button-action_edit");
     var deleteButton=taskListItem.querySelector("button.button-delete");
 
 
